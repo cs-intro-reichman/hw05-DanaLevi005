fee7f5d8783b9f0f4805536082fffa0b287d3ce9
DanaLevi005 <danapearl.levi@post.runi.ac.il>
2024-01-19 19:04:24 +0200

    test

--- GameOfLife.java
+++ GameOfLife.java
@@ -161,11 +161,11 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		for (int i = 1; i < arr.length - 1; i++) {
 			for (int j = 1; j < arr[i].length - 1; j++) {
-				System.out.printf("%3s ", arr[i][j]);
+				System.out.printf("%2s", arr[i][j]);
 
 			}
 			System.out.println();
-			
+			System.out.println();
 
 			
 		}
